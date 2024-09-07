e7a41021bd9c211dca0758fa6ffda8025bf325d2
Wade <158378370+wadevenz@users.noreply.github.com>
2024-09-07 10:40:12 +1000

    feat: gameTimeStep interval added

--- src/gameLogic.js
+++ src/gameLogic.js
@@ -2,14 +2,57 @@
 let gameTimeRemaining = 0;
 let defaultGameDuration = 120;
 let gameCountdownInterval = null;
+let startGameButton = document.getElementById("startGameButton");
+let stopGameButton = document.getElementById("stopGameButton");
+let gameUpdateInterval = null;
+let currentGameScore = 0;
+let highestGameScore = 0;
+let scoreDisplayText = document.getElementById("currentGameScore");
+let highScoreDisplayText = document.getElementById("highScoreDisplay");
+let timerDisplayText = document.getElementById("currentTimeRemaining");
+
+
+// Game Score and Timer
+function gameTimeStep(){
+    // update score displayed
+    scoreDisplayText.innerText = "Score: " + currentGameScore;
+
+    // update time remianing displayed
+    timerDisplayText.innerText = "Time Remaining: " + gameTimeRemaining;
+}
+
+
+
 
 // if (gameTimeRemaining > 0) {
 
 // }
 
-function startGame(desiredGameTime = defaultGameDuration) {
+function toggleGameControlButtons(){
+    // check gameTimeRemaining
+
+    if (gameTimeRemaining > 0) {
+        // game has started
+        startGameButton.style.display = "none";
+        stopGameButton.style.display = "inherit";
+    } else {
+        // game has finished
+        startGameButton.style.display = "inherit";
+        stopGameButton.style.display = "none";
+    }
+    // reveal or hise startGameButton
+
+    // hide or reveal stopGameButton
+}
+
+toggleGameControlButtons();
+
+function startGame(desiredGameTime = defaultGameDuration){
     gameTimeRemaining = desiredGameTime;
     console.log("Started the game. Game time remaining is now: " + gameTimeRemaining);
+
+    // toggle game controls
+    toggleGameControlButtons();
 
     gameCountdownInterval = setInterval(() => {
         gameTimeRemaining -= 1;
@@ -19,11 +62,14 @@ function startGame(desiredGameTime = defaultGameDuration) {
             // if game time remaining, stop subtracting
             clearInterval(gameCountdownInterval);
             console.log("Game has finished!");
+            stopGame();
         }
-        
-    }, 1000);
-}
 
+    }, 1000);
+
+    gameUpdateInterval = setInterval(gameTimeStep, 100)
+
+}
 
 
 // startGame(); // gameTimeRemaining becomes 120
@@ -31,16 +77,19 @@ function startGame(desiredGameTime = defaultGameDuration) {
 
 function stopGame() {
     gameTimeRemaining = 0;
+
+    // toggle game controls
+    toggleGameControlButtons();
+
     console.log("Stopped the game. Game time remaining is now: " + gameTimeRemaining)
 }
 
 
-let startGameButton = document.getElementById("startGameButton");
 startGameButton.addEventListener("click", () => {
     startGame(3);
 });
 
-let stopGameButton = document.getElementById("stopGameButton");
+
 stopGameButton.addEventListener("click", () => {
     stopGame();
 });
